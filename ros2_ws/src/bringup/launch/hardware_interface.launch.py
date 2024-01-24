4069711cce13c5dd0e5c0335a3097b877ab97977
Antoine Dangeard <antoine.dangeard@gmail.com>
2024-01-24 05:38:28 +0000

    add phidgets imu driver to ros

--- ros2_ws/src/bringup/launch/hardware_interface.launch.py
+++ ros2_ws/src/bringup/launch/hardware_interface.launch.py
@@ -1,6 +1,7 @@
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
@@ -10,4 +11,8 @@ def generate_launch_description():
             name='hardware_interface',
             output='screen',
         ),
+        ExecuteProcess(
+            cmd=[['ros2 launch phidgets_spatial spatial-launch.py']],
+            shell=True
+        )
     ])
--- ros2_ws/src/bringup/setup.py
+++ ros2_ws/src/bringup/setup.py
@@ -6,7 +6,7 @@ package_name = 'bringup'
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=find_packages('phidgets_spatial', exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
