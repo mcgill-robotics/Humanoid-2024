d2112a52318e4f04e79e03e9f40a74326e422dcc
vboucher77 <97803777+vboucher07@users.noreply.github.com>
2024-01-31 12:05:35 -0500

    more new

--- Firmware/teensy40_xl320_test/src/main.cpp
+++ Firmware/teensy40_xl320_test/src/main.cpp
@@ -1,16 +1,19 @@
 #include "Arduino.h"
-#include "control_table_xl320.h"
+#include "IPAddress.h"
 #include "Servo_XL320.h"
+#include "control_table_xl320.h"
 
-void setup()
-{
-    Serial1.begin(1000000, SERIAL_8N1_HALF_DUPLEX);
-    while(!Serial1);
+byte local_mac[] = {0xAA, 0xBB, 0xCC, 0xEE, 0xDD, 0xFF};
+IPAddress local_ip(192, 168, 1, 177);
+IPAddress agent_ip(192, 168, 1, 113);
+size_t agent_port = 8888;
 
+void setup() {
+  Serial1.begin(1000000, SERIAL_8N1_HALF_DUPLEX);
+  while (!Serial1) {
+  }
+  set_microros_native_ethernet_transports(local_mac, local_ip, agent_ip,
+                                          agent_port);
 }
 
-void loop()
-{
-
-
-}
+void loop() {}
